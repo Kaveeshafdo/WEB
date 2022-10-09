eddb3ff914a1cf48b7d15c22c610117e3a12090a
Kaveeshafdo <kaveesha.shavinda1999@gmail.com>
2022-10-09 09:38:02 +0530

    change file extention name

--- cart.php
+++ cart.php
@@ -54,38 +54,9 @@
 
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 fixed-top">
-        <div class="container">
-            <img src="img/LPA eComms.png" alt="" width="50px">
-            <h3 class="nav">LPA eComms</h3>
-
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span >  <i id="bar" class="fa fa-bars"></i></span>
-          </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto">
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.html">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="shop.html">Shop</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="about.html">About Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="contact.html">Contact Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="search.html"><i class="fa fa-search icon"></i></a>
-                        <a href="cart.html"> <i class="fa fa-shopping-basket active"></i></a>
-
-
-                    </li>
-
-
-            </div>
-        </div>
+    <?php 
+           include 'common/header.php';
+        ?> 
     </nav>
 
     <section id="" class="pt-5 mt-5 container">
@@ -210,74 +181,9 @@
 
 
     <footer class="mt-5 py-5">
-        <div class="row container mx-auto pt-5">
-            <div class="footer-one col-lg-3 col-md-6 col-12">
-
-                <p class="pt-3">LPA eComms - World biggest fashion chain offers a wide range of clothes and accessories for Men, Women and Kids. The retail store houses a range of Homeware and Lifestyle products to compliment a comprehensive family shopping experience
-                    for all our customers.
-                </p>
-                <img src="img/LPA eComms.png" width="50px" alt="">
-
-            </div>
-            <div class="footer-one col-lg-3 col-md-6 col-12 mb-3">
-                <h5 class="pb-2">Featured</h5>
-                <ul class="text-uppercase list-unstyled">
-                    <li><a href="#">Men</a></li>
-                    <li><a href="#">Women</a></li>
-                    <li><a href="#">Kids</a></li>
-                    <li><a href="#">Shoes</a></li>
-                    <li><a href="#">New Arrivals</a></li>
-                    <li><a href="#">Sports Wear</a></li>
-                </ul>
-            </div>
-
-            <div class="footer-one col-lg-3 col-md-6 col-12 mb-4">
-                <h5 class="pb-2">Contact Us</h5>
-                <div>
-                    <h6 class="text-uppercase">Address</h6>
-                    <p>No13 Road, LPA eComms</p>
-                </div>
-                <div>
-                    <h6 class="text-uppercase">Phone</h6>
-                    <p>(12)4446464646</p>
-                </div>
-                <div>
-                    <h6 class="text-uppercase">Email</h6>
-                    <p>lpaecomms@gmail.com</p>
-                </div>
-            </div>
-
-
-            <div class="footer-one col-lg-3 col-md-6 col-12 mb-2">
-                <h5 class="pb-2">Help</h5>
-                <ul class="text-uppercase list-unstyled">
-                    <li><a href="about.html">About Us</a></li>
-
-
-                </ul>
-
-
-            </div>
-        </div>
-
-        <div class="copyright mt-5">
-            <div class="row container mx-auto">
-                <div class="footer-one col-lg-3 col-md-6 col-12 mb-4">
-                    <img src="img/payment.png" alt="">
-                </div>
-
-                <div class="footer-one col-lg-4 col-md-6 col-12 text-nowrap">
-                    <p>LPA eComms 2022. All Right Reserved</p>
-                </div>
-
-                <div class="footer-one col-lg-4 col-md-6 col-12">
-                    <a href="#"><i class="fa fa-facebook"></i></a>
-                    <a href="#"><i class="fa fa-twitter"></i></a>
-                    <a href="#"><i class="fa fa-instagram"></i></a>
-                </div>
-            </div>
-
-        </div>
+    <?php   
+         include 'common/footer.php';
+       ?>
     </footer>
 
 
